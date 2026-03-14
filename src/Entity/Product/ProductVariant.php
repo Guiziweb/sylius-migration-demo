6a2a12ba793204dd6711231b281ea9b37bd10a38
camilleislasse <camille.islasse@acseo-conseil.fr>
2026-03-14 18:37:26 +0100

    fix(src): retirer les références AdyenPlugin non installé de ProductVariant
    Le plugin sylius/adyen-plugin n'est pas présent dans composer.json ni dans vendor/.
    Les références à CommodityCodeAwareInterface et CommodityCodeAwareTrait causent une
    erreur fatale au démarrage du kernel.
    
    Closes #150

--- src/Entity/Product/ProductVariant.php
+++ src/Entity/Product/ProductVariant.php
@@ -5,22 +5,15 @@ declare(strict_types=1);
 namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
-use Sylius\AdyenPlugin\Entity\CommodityCodeAwareInterface;
-use Sylius\AdyenPlugin\Entity\CommodityCodeAwareTrait;
 use Sylius\Component\Core\Model\ProductVariant as BaseProductVariant;
 use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
 use Sylius\MolliePlugin\Entity\ProductVariantInterface;
 use Sylius\MolliePlugin\Entity\RecurringProductVariantTrait;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product_variant")
- */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_product_variant')]
-class ProductVariant extends BaseProductVariant implements ProductVariantInterface, CommodityCodeAwareInterface
+class ProductVariant extends BaseProductVariant implements ProductVariantInterface
 {
-    use CommodityCodeAwareTrait;
     use RecurringProductVariantTrait;
 
     protected function createTranslation(): ProductVariantTranslationInterface
