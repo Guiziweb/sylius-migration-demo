f09912c0378a8980abcd2d0f28fdd6d0a5ba6322
camilleislasse <camille.islasse@acseo-conseil.fr>
2026-03-15 08:43:27 +0100

    Revert "chore(frontend): corriger les chemins webpack.config.js pour Sylius 2.0"
    This reverts commit 17d720ef94a6d41d4ca068da89eb58cde171b4fb.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
 const syliusBundles = path.resolve(__dirname, 'vendor/sylius/sylius/src/Sylius/Bundle/');
+const uiBundleScripts = path.resolve(syliusBundles, 'UiBundle/Resources/private/js/');
+const uiBundleResources = path.resolve(syliusBundles, 'UiBundle/Resources/private/');
 
 // Shop config
 Encore
   .setOutputPath('public/build/shop/')
   .setPublicPath('/build/shop')
-  .addEntry('shop-entry', './vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/assets/entrypoint.js')
+  .addEntry('shop-entry', './vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/private/entry.js')
   .disableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
@@ -16,6 +18,8 @@ Encore
 
 const shopConfig = Encore.getWebpackConfig();
 
+shopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
+shopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 shopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 shopConfig.name = 'shop';
 
@@ -25,7 +29,7 @@ Encore.reset();
 Encore
   .setOutputPath('public/build/admin/')
   .setPublicPath('/build/admin')
-  .addEntry('admin-entry', './vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/assets/entrypoint.js')
+  .addEntry('admin-entry', './vendor/sylius/sylius/src/Sylius/Bundle/AdminBundle/Resources/private/entry.js')
   .disableSingleRuntimeChunk()
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
@@ -34,6 +38,8 @@ Encore
 
 const adminConfig = Encore.getWebpackConfig();
 
+adminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
+adminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 adminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 adminConfig.externals = Object.assign({}, adminConfig.externals, { window: 'window', document: 'document' });
 adminConfig.name = 'admin';
@@ -53,6 +59,8 @@ Encore
 
 const appShopConfig = Encore.getWebpackConfig();
 
+appShopConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
+appShopConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 appShopConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 appShopConfig.externals = Object.assign({}, appShopConfig.externals, { window: 'window', document: 'document' });
 appShopConfig.name = 'app.shop';
@@ -72,6 +80,8 @@ Encore
 
 const appAdminConfig = Encore.getWebpackConfig();
 
+appAdminConfig.resolve.alias['sylius/ui'] = uiBundleScripts;
+appAdminConfig.resolve.alias['sylius/ui-resources'] = uiBundleResources;
 appAdminConfig.resolve.alias['sylius/bundle'] = syliusBundles;
 appAdminConfig.externals = Object.assign({}, appAdminConfig.externals, { window: 'window', document: 'document' });
 appAdminConfig.name = 'app.admin';
