8a57afa9470bfe3920f0e67928c41ade0c7d48cc
Camille Islasse <34024380+camilleislasse@users.noreply.github.com>
2026-03-15 08:18:30 +0100

    Migrer les dépendances Composer (#170)
    * chore(composer): mettre à jour les dépendances Composer vers Sylius 2.0
    
    Closes #165
    
    * chore(composer): supprimer les références adyen-plugin dans les assets
    
    Closes #165

--- assets/admin/entrypoint.js
+++ assets/admin/entrypoint.js
@@ -1,5 +1,4 @@
 import '../../vendor/sylius/mollie-plugin/assets/admin/entrypoint';
-import '../../vendor/sylius/adyen-plugin/assets/admin/entrypoint';
 
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/admin/entrypoint.js');
